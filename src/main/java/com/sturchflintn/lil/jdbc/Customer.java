51b5c6a15cfcd805f809e2c9110242553a516e02
Nick Sturch-Flint <nickdoesreviews@gmail.com>
2021-03-31 16:30:34 -0400

    Added findByID to CustomerDAO

--- src/main/java/com/sturchflintn/lil/jdbc/Customer.java
+++ src/main/java/com/sturchflintn/lil/jdbc/Customer.java
@@ -23,7 +23,7 @@ public class Customer implements DataTransferObject
     public void setFirstName(String firstName) {this.firstName = firstName;}
 
     public String getLastName() {return lastName;}
-    public void setLastName(String firstName) {this.lastName = lastName;}
+    public void setLastName(String lastName) {this.lastName = lastName;}
 
     public String getEmail() {return email;}
     public void setEmail(String email) {this.email = email;}
--- src/main/java/com/sturchflintn/lil/jdbc/JDBCExecutor.java
+++ src/main/java/com/sturchflintn/lil/jdbc/JDBCExecutor.java
@@ -14,12 +14,32 @@ public class JDBCExecutor
         try
         {
             Connection connection = dcm.getConnection();
-            Statement statement = connection.createStatement();
+            CustomerDAO customerDAO = new CustomerDAO(connection);
+            //This tests that the connection is working
+            /*Statement statement = connection.createStatement();
             ResultSet resultSet = statement.executeQuery("SELECT COUNT(*) FROM customer");
             while(resultSet.next())
             {
                 System.out.println(resultSet.getInt(1));
-            }
+            }*/
+
+            //This tests that we can add data to the db
+
+           /* Customer customer = new Customer();
+            customer.setFirstName("George");
+            customer.setLastName("Washington");
+            customer.setEmail("george.washington@wh.gov");
+            customer.setPhone("(555) 555-5555");
+            customer.setAddress("1234 Main St");
+            customer.setCity("Mount Vernon");
+            customer.setState("VA");
+            customer.setZipCode("221221");
+
+            customerDAO.create(customer); */
+
+            //This tests the Reading methods we set
+            Customer customer = customerDAO.findById(1000);
+            System.out.println(customer.getFirstName() + " " + customer.getLastName());
         }
         catch (SQLException e)
         {
